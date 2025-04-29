009fc8b762ca5a36463fc8343e210b92f8e63a07
Your Name <victor.lopesvfk11@gmail.com>
2025-04-29 09:14:05 -0300

    Add initial implementation of professor and sala structures

---
+++ TESTE-strc-professor-sala.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Materia{
+} Materia;
+
+typedef struct NodeMaterias{
+    Materia* materia;       
+    struct Node* next;
+} NodeMaterias;
+
+typedef struct Professor{
+    char nome[256];
+    NodeMaterias* lista_materias;   
+    char titulacao[50];
+} Professor;
+
+typedef struct Sala{
+    int num_sala;
+    int capacidade;
+    char tipo_sala[50];
+} Sala;
+
+int main() {
+    return 0;
+}
