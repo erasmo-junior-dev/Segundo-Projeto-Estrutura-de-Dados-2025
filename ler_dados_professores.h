a55d40bf262910649f67c118cf86eb02cc37850c
Erasmo <essj@ic.ufal.br>
2025-05-04 18:31:39 +0000

    Add input professors function

--- ler_dados_professores.h
+++ ler_dados_professores.h
@@ -4,9 +4,48 @@
 #include <stdio.h>
 #include "estruturas_e_funcoes.h"
 
-Professor* carregarDadosProfessores()
+Professor* carregarDadosProfessores(No *head)
 {
-    
+    FILE *arquivo = fopen("professores.txt", "r");
+    Professor *newProfessor = NULL;
+
+    char getLinha[100];
+    memset(getLinha, 0, sizeof(getLinha));
+
+    while (fgets(getLinha, sizeof(getLinha), arquivo) != NULL)
+    {
+        Professor *new_prof = (Professor*)malloc(sizeof(Professor));
+        char *token = strtok(getLinha, ";");
+
+        strcpy(new_prof->nome, token);
+
+        
+        while ((token = strtok(NULL, ";")) != NULL) 
+        {
+            if (strncmp(token, "TITULO:", 7) == 0) 
+            {
+                strcpy(new_prof->titulacao, token + 7);
+                new_prof->titulacao[strcspn(new_prof->titulacao, "\n")] = '\0';
+                break;
+            }
+
+            No *disciplina = SearchElement(token, head);
+
+            No *nova_disc = malloc(sizeof(No));
+            *nova_disc = *disciplina;
+            nova_disc->next = new_prof->lista_materias;
+            new_prof->lista_materias = nova_disc;
+        }
+
+        new_prof->proximo = newProfessor;
+        newProfessor = new_prof;
+
+        memset(getLinha, 0, sizeof(getLinha));
+    }
+
+
+    fclose(arquivo);
+    return newProfessor;
 }
 
 Sala* carregarDadosSalas()
--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include "estruturas_e_funcoes.h"
 #include "ler_dados_materias.h"
 #include "ler_dados_aluno.h"
+#include "ler_dados_professores.h"
 
 int main()
 {
-    No *disciplinas = CreateNewNode();
+    No *lista_de_disciplinas = CreateNewNode();
+    lista_de_disciplinas = carregarDados(lista_de_disciplinas);
 
-    disciplinas = carregarDados(disciplinas);
-    controleHistoricoAluno(disciplinas);
+    Sala *lista_de_salas = carregarDadosSalas();
+
+    controleHistoricoAluno(lista_de_disciplinas);
+
+    Professor *lista_de_professores = carregarDadosProfessores(lista_de_disciplinas);
 
     return 0;
 }
